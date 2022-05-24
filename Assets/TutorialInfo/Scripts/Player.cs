b0610ee5e9c5205329460882b1b0ac49d89e52f7
Emin Deniz <emindeniz99@gmail.com>
2022-05-25 00:36:34 +0300

    https://medium.com/@TiborUdvari/from-unity-to-testflight-faster-7d6d81b7d9c1
    https://gist.github.com/TiborUdvari/4679d636b17ddff0d83065eefa399c04
    
    https://forum.unity.com/threads/how-does-unity-cloud-build-ios-handle-modifications-to-the-xcode-project.1188190/#post-7603408

--- Assets/TutorialInfo/Scripts/Player.cs
+++ Assets/TutorialInfo/Scripts/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
 
     public float speed = 5;
 
+    private Vector2 start;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,37 @@ public class Player : MonoBehaviour
         Vector3 movement = new Vector3(x, 0, z);
 
         transform.Translate(movement * Time.deltaTime * speed);
+
+
+
+
+        foreach (var touch in Input.touches)
+        {
+
+            transform.Translate(new Vector3(touch.deltaPosition.x, 0, touch.deltaPosition.y) * Time.deltaTime);
+
+            //     if(touch.phase == TouchPhase.Began)
+            //     {
+            //         start= touch.position;
+
+            //     }else if(touch.phase== TouchPhase.Ended){
+
+
+            // Vector3 movement = new Vector3(x, 0, z);
+
+            // transform.Translate(movement * Time.deltaTime * speed);
+
+
+
+
+            //         start = Vector2.zero;
+            //     }
+        }
+
+        gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x, -3, 3), gameObject.transform.position.y, Mathf.Clamp(gameObject.transform.position.z, -5, 5));
+
+
+
     }
 
 }
